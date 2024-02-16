f0a887b5120b78d93016ed2f3968079f6fec5a8a
TatyanaVildanova <tz29@yandex.ru>
2024-02-16 19:51:55 +0300

    корректировка

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
     static let accessToken = "f4e50106-eb91-4952-85ab-e0475dc66aa4"
-    static let webview = "https://practicum.yandex.ru/ios-developer/?from=catalog"
 }
